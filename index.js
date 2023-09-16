f28c24a06392186408694d499951bf8280c3a3d9
Halceeyon <IsaacAki247@gmail.com>
2023-09-16 22:39:15 +0100

    Created a function to play the game
    - deleted function "toLower" as it unnecessarily reinvented the wheel
    
    - created function "playRound" that takes two arguments. Function uses if / else if statements to determine the winner of the rock paper scissors match between the user and the computer. Function returns "draw" if the options are the same

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 function getComputerChoice() {
-    let items = ["rock", "paper", "scissors"]
+    let items = ["rock", "paper", "scissors"];
 
     return items[Math.floor(Math.random()*items.length)];
 }
 
-// console.log(getComputerChoice());
+function playRound(playerSelection, computerSelection) {
 
-function toLower(string) {
-    return string.toLowerCase();
+    if (computerSelection === "rock" && playerSelection === "paper") {
+        return "You Win! Paper beats Rock";
+    } else if (computerSelection === "paper" && playerSelection === "rock") {
+        return "You Lose! Paper beats Rock";
+    } else if (computerSelection === "paper" && playerSelection === "scissors") {
+        return "You Win! Scissors beats paper";
+    } else if (computerSelection === "scissors" && playerSelection === "paper") {
+        return "You Lose! Scissors beats Paper";
+    } else if (computerSelection === "scissors" && playerSelection === "rock") {
+        return "You win! Rock beats Scissors";
+    } else if (computerSelection === "rock" && playerSelection === "scissors") {
+        return "You Lose! Rock beats Scissors";
+    } else {
+        return "draw";
+    }
 }
 
-// console.log(toLower("GOoDbyE"));
-
+const playerSelection = "Rock".toLowerCase();
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
