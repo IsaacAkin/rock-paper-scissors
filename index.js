f4664c49923b324d749f4ec6140b64c9c8a977e8
Halceeyon <IsaacAki247@gmail.com>
2023-09-21 16:53:31 +0100

    Created a function to detect the winner based on who won the most games
    - created the variables playerCounter and computerCounter to store how many rounds each player has one. Set their initial values to 0.
    
    - created the function detectWinner that takes the two created variables as parameters. Function outputs who won based on who has a higher score or a draw if the scores are equal
    
    - edited the playRound function to add 1 to each respective variable counter based on who won via the matchup outcome
    
    - added comments to explain what each function does
    
    - added defer to the script tag in index.html

--- index.js
+++ index.js
@@ -1,25 +1,49 @@
+/** Counters for determining who has the higher score */
+let playerCounter = 0;
+let computerCounter = 0;
+
+/** Function that randomly generates the computers choice */
 function getComputerChoice() {
     let items = ["rock", "paper", "scissors"];
 
     return items[Math.floor(Math.random()*items.length)];
 }
 
+/** Function that uses if / else if statements to determine the winner of the match */
 function playRound(playerSelection, computerSelection) {
 
     if (computerSelection === "rock" && playerSelection === "paper") {
+        playerCounter += 1;
         return "You Win! Paper beats Rock";
     } else if (computerSelection === "paper" && playerSelection === "rock") {
+        computerCounter += 1;
         return "You Lose! Paper beats Rock";
     } else if (computerSelection === "paper" && playerSelection === "scissors") {
+        playerCounter += 1;
         return "You Win! Scissors beats paper";
     } else if (computerSelection === "scissors" && playerSelection === "paper") {
+        computerCounter += 1;
         return "You Lose! Scissors beats Paper";
     } else if (computerSelection === "scissors" && playerSelection === "rock") {
+        playerCounter += 1;
         return "You win! Rock beats Scissors";
     } else if (computerSelection === "rock" && playerSelection === "scissors") {
+        computerCounter += 1;
         return "You Lose! Rock beats Scissors";
     } else {
         return "draw";
+    }
+}
+
+/** Function that detects the winner based on who has the higher score at the end of the round */
+function detectWinner(playerCounter, computerCounter) {
+
+    if (playerCounter === computerCounter) {
+        return `Draw! Player: ${playerCounter} - Computer: ${computerCounter}`
+    } else if (playerCounter > computerCounter) {
+        return `Congratulations! You Won! Player: ${playerCounter} - Computer: ${computerCounter}`
+    } else if (computerCounter > playerCounter) {
+        return `Sorry, You Lose! Computer: ${computerCounter} - Player: ${playerCounter}`
     }
 }
 
